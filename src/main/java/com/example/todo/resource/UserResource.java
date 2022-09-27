98c295230b86399c8278609d3512c1aa276f322a
jevonne876 <jevonne63@gmail.com>
2022-09-26 19:02:40 -0500

    ResponseEntity added to register user method..

--- src/main/java/com/example/todo/resource/UserResource.java
+++ src/main/java/com/example/todo/resource/UserResource.java
@@ -4,9 +4,12 @@ import com.example.todo.model.User;
 import com.example.todo.service.UserService;
 import com.example.todo.utility.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping(value = "/api/v1/")
@@ -25,7 +28,9 @@ public class UserResource {
     }
 
     @PostMapping(value = "post/register-new-user")
-    public User registerUser(@RequestBody User user) {
-        return userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail(), user.getPassword());
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User newUser = new User();
+        newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail(), user.getPassword());
+        return new ResponseEntity<>(newUser, OK);
     }
 }
